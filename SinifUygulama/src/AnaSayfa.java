66d1de9fe753bfcee4b24700a88db6fbcd21dd0f
unaly <unalyaser3@gmail.com>
2025-02-06 18:32:05 +0300

    Ana sayfa sinifi oluşturuldu

---
+++ SinifUygulama/src/AnaSayfa.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class AnaSayfa {
+    JFrame frame = new JFrame();
+    AnaSayfa(){
+        frame.setSize(500,500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+
+    }
+}
